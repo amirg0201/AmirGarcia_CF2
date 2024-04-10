e3e59a01061e1a38e4432b108d5c9ec4928b81a6
(Estudiante) Amir Alejandro García Villamarín <amir.garcia@udla.edu.ec>
2024-04-10 10:48:05 -0500

    Agregar archivos de proyecto.

---
+++ AmirGarcia_EjercicioCF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmirGarcia_EjercicioCF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

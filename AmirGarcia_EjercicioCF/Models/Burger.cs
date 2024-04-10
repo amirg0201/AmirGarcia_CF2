a8ec32a299f9f7462ea89fcf7f8c0e8ccc3eea27
(Estudiante) Amir Alejandro García Villamarín <amir.garcia@udla.edu.ec>
2024-04-10 11:08:48 -0500

    Creacion de la clase Burger

---
+++ AmirGarcia_EjercicioCF/Models/Burger.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmirGarcia_EjercicioCF.Models
+{
+    public class Burger
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+    }
+}
